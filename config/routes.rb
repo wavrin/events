1680a2fa4ccfbfe65df24a3b858c3a787ff902f2
Christopher Wavrin <wavrin@gmail.com>
2014-01-25 11:48:05 -0600

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Events::Application.routes.draw do
+
+  root "events#index"
+  # get "events"     => "events#index"
+  # get "events/:id" => "events#show", as: "event"
+  # get "events/:id/edit" => "events#edit", as: "edit_event"
+  # patch "events/:id" => "events#update"
+  resources :events do
+  	resources :registrations
+  end
+end
